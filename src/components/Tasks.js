46c2e6899dadc1baf7b803711a73178174445806
ericaang <erica31@yahoo.com>
2024-07-08 17:54:41 +0800

    clean up codes

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,38 +1,17 @@
-import React, { useContext, useEffect, useState } from "react";
-//without react-redux
-//import StoreContext from "../context/storeContext";
+import React, { useEffect } from "react";
 
 //with react-redux
 import { useDispatch, useSelector } from "react-redux";
 import { loadTask } from "../store/task";
 
 const Tasks = () => {
-  //const store = useContext(StoreContext);
-  //const [tasks, setTasks] = useState([]);
-
   const { tasks, loading } = useSelector((state) => state.tasks);
   const dispatch = useDispatch();
-
-  /* useEffect(() => {
-    store.dispatch(loadTask());
-
-    const unsubscribe = store.subscribe(() => {
-      const storeTasks = store.getState().tasks.tasks;
-      if (tasks !== storeTasks) {
-        setTasks(storeTasks);
-      }
-    });
-
-    return () => {
-      unsubscribe();
-    }; 
-  }, []); */
 
   useEffect(() => {
     dispatch(loadTask());
   }, []);
 
-  console.log(tasks);
   return (
     <>
       {loading ? (
